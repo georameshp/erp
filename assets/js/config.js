fc093ffc9a4fc0ce9dcacb06a8e10e0c51e45a84
Geo Ramesh <geo.techno20c@gmail.com>
2026-07-01 15:59:33 +0400

    Add REST Countries API configuration
    Added REST Countries API configuration to config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -6,6 +6,11 @@ window.ERP_CONFIG = {
 
   APP_ID: "drive-erp-adminlte",
   APP_FOLDER_NAME: "ERP-App-Data",
+
+  // REST Countries registered API. This is used by the setup page Select2 fields.
+  // Note: because this is a GitHub Pages/browser-only app, this key is visible in browser source.
+  RESTCOUNTRIES_API_BASE: "https://api.restcountries.com/countries/v5",
+  RESTCOUNTRIES_API_KEY: "rc_live_31966f39f6614710913ab07636480955",
   SCOPES: [
     "openid",
     "email",
