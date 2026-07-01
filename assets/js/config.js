47f5c1911f6d99d9c7f603d69130fec563571021
Geo Ramesh <geo.techno20c@gmail.com>
2026-07-01 14:38:38 +0400

    Update Google OAuth 2.0 Client ID in config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -2,7 +2,7 @@
 window.ERP_CONFIG = {
   // TODO: Replace with your Google OAuth 2.0 Web Client ID.
   // Google Cloud Console > APIs & Services > Credentials > OAuth client ID > Web application.
-  GOOGLE_CLIENT_ID: "PASTE_YOUR_GOOGLE_WEB_CLIENT_ID_HERE",
+  GOOGLE_CLIENT_ID: "395438751245-dbpqd2col2fdeo04017n2qardhj4kk1c.apps.googleusercontent.com",
 
   APP_ID: "drive-erp-adminlte",
   APP_FOLDER_NAME: "ERP-App-Data",
